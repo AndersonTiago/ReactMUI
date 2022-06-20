20f4194c2b9f128473fa098d0fe2b36f592aecad
Anderson Tiago <andersonbonamim@gmail.com>
2022-06-20 20:14:51 -0300

    Criando botão de dark-mode

--- src/shared/components/manu-lateral/MenuLateral.tsx
+++ src/shared/components/manu-lateral/MenuLateral.tsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material"
 import { useMatch, useNavigate, useResolvedPath } from "react-router-dom"
-import { useDrawerContext } from "../../contexts"
+import { useAppThemeContext, useDrawerContext } from "../../contexts"
 
 interface IChildren {
     children?: React.ReactNode
@@ -40,6 +40,7 @@ export const MenuLateral: React.FC<IChildren> = ({ children }) => {
     const theme = useTheme()
     const smDown = useMediaQuery(theme.breakpoints.down('sm')) // verificando se a tela é pequena
     const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext()
+    const { toggleTheme } = useAppThemeContext()
 
     return (
         <>
@@ -64,6 +65,16 @@ export const MenuLateral: React.FC<IChildren> = ({ children }) => {
                                     onClick={smDown ? toggleDrawerOpen : undefined}
                                 />
                             )))}
+                        </List>
+                    </Box>
+                    <Box>
+                        <List component="nav">
+                            <ListItemButton onClick={toggleTheme}>
+                                <ListItemIcon>
+                                    <Icon>dark_mode</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Alternar tema" />
+                            </ListItemButton>
                         </List>
                     </Box>
                 </Box>
