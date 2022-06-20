41f925a8776e749665756126f62aa728ab9264cc
Anderson Tiago <andersonbonamim@gmail.com>
2022-06-20 19:33:01 -0300

    Configurando Header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@mui/material';
 import { useEffect } from 'react';
-import { Routes, Route, Link, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Dashboard } from '../pages';
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
@@ -12,18 +12,13 @@ export const AppRoutes = () => {
                 icon: 'home',
                 label: 'Página inicial',
                 path: 'Página inicial'
-            },
-            {
-                icon: 'star',
-                label: 'cidades',
-                path: 'cidades'
             }
         ])
     }, [])
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Toggle Drawer</Button>} />
+            <Route path="/pagina-inicial" element={<Dashboard />} />
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     );
