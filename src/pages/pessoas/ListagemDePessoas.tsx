d864d31299caa55162ff8ad594d274e69a335824
Anderson Tiago <andersonbonamim@gmail.com>
2022-07-12 20:10:05 -0300

    Criando tabela de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,30 +1,41 @@
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material"
 import { useSearchParams } from "react-router-dom"
+
 import { FerramentasDaListagem } from "../../shared/components"
 import { useDebounce } from "../../shared/hooks"
 import { LayoutBaseDePagina } from "../../shared/layouts"
-import { PessoasService } from "../../shared/services/api/pessoas/PessoasService"
+import { IListagemPessoa, PessoasService } from "../../shared/services/api/pessoas/PessoasService"
+import { Environment } from "../../shared/environment"
 
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
-  const { debounce } = useDebounce(3000)
+  const { debounce } = useDebounce()
 
-
+  const [rows, setRows] = useState<IListagemPessoa[]>([])
+  const [totalCount, setTotalCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
   useEffect(() => {
+    setIsLoading(true)
+
     debounce(() => {
       PessoasService.getAll(1, busca)
         .then((result) => {
+          setIsLoading(false)
           if (result instanceof Error) {
             alert(result.message)
             return
+          } else {
+            setRows(result.data)
+            setTotalCount(result.totalCount)
           }
-          console.log(result)
+
 
         });
     })
@@ -42,6 +53,46 @@ export const ListagemDePessoas: React.FC = () => {
         />
       }
     >
+
+      <TableContainer sx={{ m: 1, width: "auto" }} component={Paper} variant="outlined">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+
+          </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}
+
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant="indeterminate" />
+                </TableCell>
+              </TableRow>
+            )}
+
+
+          </TableFooter>
+        </Table>
+      </TableContainer>
 
     </LayoutBaseDePagina>
   )
